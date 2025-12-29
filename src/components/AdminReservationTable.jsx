e3c39cf7b9285dd41af4908b6c99310b66ded3c3
pavani-2023 <kakumanupavani251@gmail.com>
2025-12-30 00:21:34 +0530

    first commit

---
+++ src/components/AdminReservationTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminReservationTable() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/BookingModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BookingModal() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
